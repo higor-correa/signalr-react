3f2dbec0ea42690d035f49eec6210b0d3a31bd67
Higor Correa <higor.dev@gmail.com>
2019-08-16 12:16:21 -0300

    Calling hub from another controller

---
+++ SignalRHost/SignalRHost/Controllers/MessagesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRHost.Hubs;
+using System.Threading.Tasks;
+
+namespace SignalRHost.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessagesController : ControllerBase
+    {
+        private IHubContext<ChatHub> _chatHubContext;
+
+        public MessagesController(IHubContext<ChatHub> chatHubContext)
+        {
+            _chatHubContext = chatHubContext;
+        }
+
+        [HttpPost("send")]
+        public async Task<IActionResult> SendAsync(MessageRequest messageRequest)
+        {
+            var user = messageRequest.User;
+            var message = messageRequest.Message;
+            await _chatHubContext.Clients.All.SendAsync("ReceiveMessage", user, message);
+            return Ok();
+        }
+
+
+    }
+
+    public class MessageRequest
+    {
+        public string Message { get; set; }
+        public string User { get; set; }
+    }
+}
